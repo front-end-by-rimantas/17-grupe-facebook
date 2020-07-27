dc4dd128e6d3b1d1c90ba9ed050811ca98c371d5
Rimantas <belauzas@yahoo.com>
2020-07-27 10:29:39 +0300

    See more logic update

--- js/renderText.js
+++ js/renderText.js
@@ -1,10 +1,14 @@
 function renderText( text ) {
-    if ( text.length <= 60 ) {
+    const shortText = 60;
+    const mediumText = 300;
+    const largeText = 360;
+
+    if ( text.length <= shortText ) {
         return `<p class="big-text">${text}</p>`;
-    } else if ( text.length <= 300 ) {
+    } else if ( text.length <= largeText ) {
         return `<p>${text}</p>`;
     } else {
-        return `<p class="short-text">${text.substr(0, 300)}... <span class="read-more">see more</span></p>
+        return `<p class="short-text">${text.substr(0, mediumText)}... <span class="read-more">see more</span></p>
                 <p class="full-text hidden">${text} <span class="read-less">see less</span></p>`;
     }
 }
